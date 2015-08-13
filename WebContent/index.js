bd2a69e6cb166ae1e5e6b7001aed33af070a14cd
Len Mahaffey <bozziley@gmail.com>
2015-08-12 18:16:23 -0600

    Cleaned up javascript

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,68 +1,20 @@
-function loadStates(){
-  	var states = document.getElementById("map");
-  	var SVGDoc = states.contentDocument;
-  	var allStates = SVGDoc.getElementById("states");
- 	allStates.style.fill = "grey";
- 	SVGDoc.getElementById("AL").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("AZ").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("AR").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("CA").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("CO").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("CT").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("DC").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("DE").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("FL").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("GA").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("ID").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("IA").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("IL").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("IN").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("KS").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("KY").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("LA").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("MA").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("MD").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("ME").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("MI").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("MN").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("MO").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("MS").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("MT").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("NC").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("ND").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("NE").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("NH").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("NJ").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("NM").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("NV").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("NY").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("OH").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("OK").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("OR").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("PA").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("RI").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("SC").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("SD").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("TN").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("TX").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("UT").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("VT").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("VA").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("WA").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("WI").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("WV").onclick = function(){ newWindow(this) };
- 	SVGDoc.getElementById("WY").onclick = function(){ newWindow(this) };
+function showCounties(){
+	map.style.display = "none";
+	map.setAttribute("data", "images/counties.svg");
+	map.style.display = "";
 }
 
-function newWindow(target){
-	//target.style.fill = "red";
-	var newWindow = window.open('http://www.google.com', 
-			"mywindow",
-			"menubar=0, resizable=0, width=500,height=500");
+function showStates(){
+	map.style.display = "none";
+	map.setAttribute("data", "images/states.svg");
+	map.style.display = "";
 }
 
-function colorState(state){
-	SVGDoc.getElementById(state).style.fill = "red";
+function getResults(){
+	states = map.contentDocument.getElementsByClassName('state');
+	for(var i = 0; i < states.length; i++){
+		ajaxRequest('stateWinner', states.item(i).id, '2000');
+	}	
 }
 
 function ajaxRequest(action, state, year, FIPS){
@@ -98,82 +50,3 @@ function ajaxRequest(action, state, year, FIPS){
 	request.setRequestHeader("Accept", "application/json");
 	request.send(params);
 }
-
-function getResults(){
-	var statesArray = ["AL","AZ","AR","CA","CO","CT","DE","FL","GA","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WI","WV","WY"];
-	for (i = 0; i < statesArray.length; i++){
-		ajaxRequest('stateWinner', statesArray[i], '2000');
-	}
-}
-/*
-function loadCounties(){
-	var mapDoc = document.getElementById("map");
-	var SVGDoc = mapDoc.contentDocument;
-	var allStates = SVGDoc.getElementById("counties");
-	allStates.style.fill = "grey";
-	SVGDoc.getElementById("AL").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("AZ").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("AR").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("CA").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("CO").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("CT").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("DC").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("DE").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("FL").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("GA").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("ID").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("IA").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("IL").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("IN").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("KS").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("KY").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("LA").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("MA").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("MD").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("ME").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("MI").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("MN").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("MO").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("MS").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("MT").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("NC").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("ND").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("NE").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("NH").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("NJ").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("NM").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("NV").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("NY").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("OH").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("OK").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("OR").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("PA").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("RI").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("SC").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("SD").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("TN").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("TX").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("UT").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("VT").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("VA").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("WA").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("WI").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("WV").onclick = function(){ newWindow(this) };
-	SVGDoc.getElementById("WY").onclick = function(){ newWindow(this) };
-}
-
-
-function switchCounties(){
-	document.getElementById("map").style.display = "none";
-	document.getElementById("map").setAttribute("data", "images/counties.svg");
-	document.getElementById("map").style.display = "";
-	document.getElementById("map").onLoad = "loadCounties()"
-}
-
-function switchStates(){
-	document.getElementById("map").style.display = "none";
-	document.getElementById("map").setAttribute("data", "images/states.svg");
-	document.getElementById("map").style.display = "";
-	document.getElementById("map").onLoad = "loadStates()"
-}
-*/
